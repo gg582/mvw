e3866487dfc994cbd57d0d30dfbed7f8eb5a5ed9
fatinul <fatinulhaziq22@gmail.com>
2026-01-21 19:15:37 +0900

    fix: Add Worldwide in loading spinner
    -bump v0.4.3
    -Add 'worldwide' in boxoffice loading dialog

--- mvw/movie.py
+++ mvw/movie.py
@@ -68,7 +68,7 @@ class MovieManager:
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         }
 
-        with console.status("[bold]Searching Box Office Data...", spinner="earth"):
+        with console.status("[bold]Searching Worldwide Boxoffice Data...", spinner="earth"):
             try:
                 response = requests.get(url, headers=headers)
                 soup = BeautifulSoup(response.text, 'html.parser')
